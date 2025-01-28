f9fdd3ab751f66ae49bbadf5d7a78419d5a27829
ZHADOW999 <babalolav9@gmail.com>
2025-01-28 22:31:47 +0100

    Configure ESLint rules for React and TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,29 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable unused eslint-disable directive warnings
+      "eslint-comments/no-unused-disable": "off",
+
+      // Disable React hook naming rule
+      "react-hooks/rules-of-hooks": "off",
+
+      // Disable unused variables for both TS and JS
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "off", // Or use "warn" instead of "off" to just display warnings
+        {
+          argsIgnorePattern: "^_", // Ignore unused function arguments starting with "_"
+          varsIgnorePattern: "^_", // Ignore unused variables starting with "_"
+          ignoreRestSiblings: true, // Ignore unused destructured variables
+        },
+      ],
+
+      // Disable unescaped entities warnings
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
